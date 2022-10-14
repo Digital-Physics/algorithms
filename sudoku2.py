d98f90007c97a41de75f99a6c276e5ed1dc4a6e7
Jonathan Khanlian <jdkhanlian@gmail.com>
2022-10-14 10:52:52 -0400

    sudoku2 solution cleaned up w/ additional clarification on backtracking

--- sudoku2.py
+++ sudoku2.py
@@ -1,6 +1,6 @@
 # thanks Computerphile and Prof. Thorsten Altenkirch
-print("think DFS growing odometer (w/ faster numbers on right) password attempts")
-print("once you exhaust all possibilities, backtrack and turn that back")
+print("think: ~DFS growing odometer (w/ faster numbers on right). can you grow it to length m*n through repeated is_valid guesses?")
+print("once you exhaust all possibilities at one length, backtrack, roll your odometer, and keep searching")
 
 
 class SudokuSolver:
@@ -12,26 +12,34 @@ class SudokuSolver:
         print(self.grid)
         for row in range(9):
             for col in range(9):
-                print("look at cell", row, col)
+                print("look at cell to see if it's blank/0", row, col)
                 if self.grid[row][col] == 0:
                     for n in range(1, 10):
                         print(f"try {n} in row,col: {row},{col}")
                         if self.is_valid(row, col, n):
-                            print("it works. use it.")
+                            print("it's a valid move. use it.")
                             self.grid[row][col] = n
-                            print("We now just need to solve the same but smaller problem. Recursive call to solve().")
+                            print("We have updated a value in the sudoku matrix we are for looping over to find first blank cell")
+                            print("We now just need to start at the beginning of that loop. Recursive call to solve().")
+                            print("It's the same problem to solve, but smaller. we are now one step closer to finding a solution")
                             self.solve()
-                            print("Dead end. solve() didn't kick off another solve() or get to the end. Our assumption was wrong.")
-                            print("Backtrack. Erase speculations we kow are wrong now. Set cell back to 0.")
-                            print("Continue our for loop search until those possibilities run out.")
-                            print("the for loop keeps increasing our flattened-sudoku-odometer-solution...")
-                            print("and we've check all shorter & lower numbers already.")
-                            self.grid[row][col] = 0
-                    print("now we pick up after the recursive solve() call didn't find a solution")
+                    print("Dead end. solve() didn't kick off another solve() (or if got out of for loop to the end/solution).")
+                    print("=> Our assumption was wrong.")
+                    print(f"we exhausted all 9 possibilities for cell {row, col}")
+                    print("Backtrack. Set cell back to 0 & increment the last move that lead to dead end. The growing odometer turns over")
+                    print("The odometer shrinks for only a moment when cell set to 0, assuming only one num turns over (not followed by 9)")
+                    print("After the 'return', we'll continue where our for loop left off, one solve() level up")
+                    print("our flattened-sudoku-odometer analogy is also like looking through a dictionary in order")
+                    print("we check solutions 7, 8, 1, everything else, before we check solutions starting with 7, 8, 2, ...")
+                    self.grid[row][col] = 0
+                    print("now we pick up at where the solve()'s for loop was one nested level up, before we called a dead end solve()")
                     return
-        print("save solution (we essential assume the first sudoku solution is wrong and backtrack; solve() wasn't kicked off again.)")
+        print("we got through our for loop so we must be to the end and found a valid solution")
+        print("save solution (we essential assume the first sudoku solution is wrong and let the outer solve()s keep going")
         print("at the end, we will output the last saved sudoku solution out of all possibilities")
         self.solution = [row[:] for row in self.grid]
+        print("a solution:", self.solution)
+        print("last recursive solve() call is popped off the stack and we go up a level to previous solve()")
 
     def is_valid(self, y: int, x: int, n: int) -> bool:
         for i in range(9):  # check cols
