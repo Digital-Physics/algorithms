e3a6269e58b8891f8e9eb02aac985a3134456af4
Jonathan Khanlian <jdkhanlian@gmail.com>
2024-05-02 10:28:24 -0400

    added radix sort (LSD)

--- counting_sort.py
+++ counting_sort.py
@@ -1,28 +1,65 @@
 from typing import List
 
 def counting_sort(array: List[int]) -> List[int]:
-    """a stable sorting algorithm that works well for a small number keys that it will be sorted by.
-    we work with a small range of integers here, but you could imagine tuples objects with one value being the key,
-    and all the tuple values don't have too many possible keys."""
+    """a stable sorting algorithm that works well for a small amount of keys it will be sorted by.
+    we work with a small range of integers here where the item to be sorted is the key, 
+    but you could imagine tuples objects with one value being the key. This would be a good sorting algorithm
+    for long lists without too many possible keys. If the number of keys equals the number of 
+    items to sort, the time complexity jumps from O(n + max_val), something linear when the size of the max_val
+    is held constant, to quadratic."""
 
-    # initialize our mutable counter array which starts with counts => PDF => CDF => insert index-updated CDF
+    # initialize our mutable counter array which which evolves: counts => PDF => CDF => decremented CDF
     max_val = max(array)
     counter = [0] * (max_val + 1)
 
-    # create PDF where x is the number (or letter) and y is the count or density
+    # create a discrete PDF of integers (or letters or enumerable keys)
     for i in range(len(array)):
-        counter[array[i]] += 1
+        counter[array[i]] += 1  # radix_sort_place will be 1, 10, 100, 1000, etc.
 
-    # create the cumulative count, the CDF
+    # create the cumulative count, the discrete CDF
     for i in range(1, len(counter)):
         counter[i] = counter[i] + counter[i-1]
 
+    # initialize output that we'll populate
     output = [None for _ in range(len(array))]
 
-    # start from end of original array (if you study the algo, you'll realize this is stable)
+    # start from end of original array and work backwards to populate output array 
+    # if you think about this algorithm, you'll realize this makes the sorting stable) 
     for i in range(len(array) - 1, -1, -1):
         output[counter[array[i]] - 1] = array[i]
         counter[array[i]] -= 1
+
+    return output
+
+
+def counting_sort_for_radix_sort(array: List[int], radix_sort_place: int = 1) -> List[int]:
+    """a stable sorting algorithm that works well for a small amount of keys it will be sorted by.
+    we work with a small range of integers here where the item to be sorted is the key, 
+    but you could imagine tuples objects with one value being the key. This would be a good sorting algorithm
+    for long lists without too many possible keys. If the number of keys equals the number of 
+    items to sort, the time complexity jumps from O(n + max_val), something linear when the size of the max_val
+    is held constant, to quadratic. To understand why we have the radix sort variable, read radix_sort.py"""
+
+    # initialize our mutable counter array which which evolves: counts => PDF => CDF => decremented CDF
+    max_val = max(array) if radix_sort_place == 1 else 9  # we assume radix sort is done on base 10 numbers
+    counter = [0] * (max_val + 1)
+
+    # create a discrete PDF of integers (or letters or enumerable keys)
+    for i in range(len(array)):
+        counter[(array[i]//radix_sort_place) % 10] += 1  # radix_sort_place will be 1, 10, 100, 1000, etc.
+
+    # create the cumulative count, the discrete CDF
+    for i in range(1, len(counter)):
+        counter[i] = counter[i] + counter[i-1]
+
+    # initialize output that we'll populate
+    output = [None for _ in range(len(array))]
+
+    # start from end of original array and work backwards to populate output array 
+    # if you think about this algorithm, you'll realize this makes the sorting stable) 
+    for i in range(len(array) - 1, -1, -1):
+        output[counter[(array[i]//radix_sort_place) % 10] - 1] = array[i]
+        counter[(array[i]//radix_sort_place) % 10] -= 1
 
     return output
 
---
+++ radix_sort.py
@@ -0,0 +1,37 @@
+from counting_sort import counting_sort_for_radix_sort
+
+# Radix, in the context of technology and computing, refers to the base of a number system. 
+# It's the number of unique digits (including zero) used to represent numbers in a positional numeral system.
+# In base 10, there are only 10 possible keys, and that's why Radix sort leverages counting sort (see notes in counting_sort.py).
+
+def LSD_radix_sort(arr):
+    """We will sort the list based on the least significant digit first, then sort on the next LSD, and so on.
+    This works because counting sort is a stable sorting algorithm. Even though items based on the ones place
+    will all get reshuffled in the next round, they will still keep their relative order if they have the same 10s digit.
+    This analogy holds for subsequent sorting rounds as well."""
+    max_num = max(arr) # this is for our while loop, not for our range of keys which we assume is 10, digits 0-9
+    radix_sort_place = 1
+
+    while max_num // radix_sort_place > 0: # this will loop for as many digits as we have in the longest number
+        arr = counting_sort_for_radix_sort(arr, radix_sort_place)
+        radix_sort_place *= 10
+
+    return arr
+
+
+def MSD_radix_sort(arr):
+    """although it is more intuitive that this approach would work, and it represents how a human might do radix sort,
+    the code is a little more convoluted because of recursion. we'd need to split the sorted ties into buckets and further
+    sort them by the next MSD."""
+    max_num = max(arr) 
+    radix_sort_place = len(str(max_num)) - 1
+
+    ...
+
+
+if __name__ == "__main__":
+    arr = [170, 45, 75, 90, 802, 24, 2, 66]
+    sorted_arr = LSD_radix_sort(arr)
+    sorted_arr2 = MSD_radix_sort(arr)
+    print("Sorted array is:", sorted_arr)
+    print("Sorted array is:", sorted_arr2)
