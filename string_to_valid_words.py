9e13e65bca8afcb842de7e2cfd9046cb1c0f89f9
Jonathan Khanlian <jdkhanlian@gmail.com>
2024-05-21 14:29:45 -0400

    fixed backtracking

--- string_to_valid_words.py
+++ string_to_valid_words.py
@@ -2,52 +2,45 @@ from typing import Set, List, Tuple, Optional
 
 def string_to_valid_words(s: str, dictionary: Set[int]) -> List[str]:
     """there is a valid set of words in the string. return a valid string with spaces inserted.
-    we'll use backtracking, the growing and shrinking odometer."""
-    def is_valid(s, dictionary, start_idx) -> Tuple[bool, Optional[str], int]:
-        """returns True/False, word, and next starting idx"""
-        curr_idx = start_idx + 1
+    we'll use backtracking (DFS), the growing and shrinking of an odometer that will eventually 
+    grow to our solution length and be the solution."""
 
-        while curr_idx < (len(s) + 1) and (s[start_idx: curr_idx] not in dictionary or s[start_idx: curr_idx] in words_tried[start_idx]):
-            curr_idx += 1
+    def get_next_word(s, dictionary, start_idx) -> Tuple[bool, Optional[str], int]:
+        """returns True/False depending on whether it finds the next word to append, the next word to append (or the rest of the string), and the end idx."""
+        end_idx = start_idx + 1 # +1 because it is used as the end idx [start_idx: end_idx]
 
-        # did it hit end (and if so was it not a word) or did we just find the next word?
-        if curr_idx == (len(s) + 1):
-            if s[start_idx: curr_idx] in words_tried[start_idx]:
-                print("backtrack (from, to):", start_idx, start_idx_stack[-2])
-                output.pop()
-                start_idx_stack.pop()
-                curr_idx = start_idx_stack[-1] # backtrack (take a step back because we've exhausted the current/LSD in our odometer)
-                return False, None, curr_idx
-            
-            words_tried[start_idx].add(s[start_idx: curr_idx])
-
-            if s[start_idx: curr_idx] not in dictionary:
-                curr_idx = start_idx  # spin the odometer
-                return False, None, curr_idx
-            else:
-                return True, s[start_idx: curr_idx], curr_idx  # grow odometer
-        else:
-            words_tried[start_idx].add(s[start_idx: curr_idx])
-            return True, s[start_idx: curr_idx], curr_idx
+        # find next legal word's ending index (or end on the last idx)
+        while end_idx < (len(s) + 1) and (s[start_idx: end_idx] not in dictionary or s[start_idx: end_idx] in words_tried[start_idx]):
+            end_idx += 1
         
-    output = []
+        if s[start_idx: end_idx] not in dictionary:
+            return False, s[start_idx: end_idx], end_idx
+        else:
+            return True, s[start_idx: end_idx], end_idx  # grow odometer
+        
     # for backtracking
+    # the growing and shrinking "odometer" is the list of words we've matched so far
+    # the Least Significant Digit of this growing and shrinking odometer is the last word in our list we are constructing in odometer
+    odometer = []
     start_idx = 0
     start_idx_stack = [0]
     words_tried = [set() for _ in range(len(s))] # List[Set[str]]
 
-    # we need to backtrack if we hit a dead end
-    # the growing and shrinking "odometer" is the list of words we've matched so far
-    # the Least Significant Digit of this growing and shrinking odometer is the last word in our list we are constructing
     while start_idx < len(s):
-        word_found_bool, next_word, start_idx = is_valid(s, dictionary, start_idx)
+        word_found_bool, next_word, start_idx = get_next_word(s, dictionary, start_idx)
 
         if word_found_bool:
+            words_tried[start_idx_stack[-1]].add(next_word)
             start_idx_stack.append(start_idx)
-            output.append(next_word)
-    
-    return " ".join(output)
-        
+            odometer.append(next_word)
+        else:
+            # backtrack
+            odometer.pop()
+            start_idx_stack.pop()
+            start_idx = start_idx_stack[-1]
+
+        print("curr output odometer:", odometer)
+    return " ".join(odometer)   
 
 if __name__ == "__main__":
-    print(string_to_valid_words("whoami", {"who", "am", "i", "bob", "wh"}))
+    print(string_to_valid_words("whoami", {"who", "am", "i", "bob", "wh", "oa"}))
